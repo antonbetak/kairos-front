f3890cf1f10021cf094774184844c271e4e0dfcc
Anton Betak Licea <anton@Antons-MacBook-Air.local>
2026-05-07 13:29:18 -0600

    feat: inicializar esqueleto de app React Native

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
